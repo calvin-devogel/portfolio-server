eee1eaa5931abbb558f48045644c8e148b905b10
Calvin D. <cdevogel25@gmail.com>
2026-02-12 20:13:54 -0600

    test unauthorized users attempting to patch

--- tests/api/messages/patch.rs
+++ tests/api/messages/patch.rs
@@ -20,7 +20,7 @@ struct MessagesResponse {
 }
 
 #[tokio::test]
-async fn admin_can_patch_messages() {
+async fn authorized_user_can_patch_messages() {
     let app = spawn_app().await;
     app.test_user.login(&app).await;
 
@@ -63,4 +63,40 @@ async fn admin_can_patch_messages() {
 
     assert_eq!(message_is_read.message_id, message_id);
     assert_eq!(message_is_read.read_message, Some(true));
+}
+
+#[tokio::test]
+async fn unauthorized_users_cannot_patch_messages() {
+    let app = spawn_app().await;
+    app.test_user.login(&app).await;
+
+    // act 1: send a message
+    let message = serde_json::json!({
+        "email": "fake@email.com",
+        "sender_name": "John Doe",
+        "message_text": "Message text."
+    });
+    app.post_message(&message).await;
+
+    // act 2: get the posted messages
+    let response_body = app.get_messages().await;
+
+    let messages_response: MessagesResponse = response_body
+        .json()
+        .await
+        .expect("Failed to parse messages response");
+
+    let message_id = messages_response.messages[0].message_id;
+
+    let patch_body = MessageToPatch {
+        message_id,
+        read: true,
+    };
+
+    // act 3: logout
+    app.post_logout().await;
+
+    // act 4: attempt to patch
+    let response = app.patch_message(&patch_body).await;
+    assert_eq!(response.status().as_u16(), 401);
 }
