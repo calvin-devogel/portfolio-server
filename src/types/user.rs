bd4a73a110e060d5c20a05a010b9bbc8bcdad7f9
Calvin D. <cdevogel25@gmail.com>
2026-04-03 11:23:45 -0500

    userRole enum snake_case serde rename

--- src/types/user.rs
+++ src/types/user.rs
@@ -10,6 +10,7 @@ pub enum UserActionType {
 
 #[derive(PartialEq, Eq, Debug, serde::Serialize, serde::Deserialize, sqlx::Type)]
 #[sqlx(type_name = "user_role", rename_all = "snake_case")]
+#[serde(rename_all = "snake_case")]
 pub enum UserRole {
     Admin,
     User,
